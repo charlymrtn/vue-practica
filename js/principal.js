4dcfe6b3bf8a03bea0bf8fe0d04e6a485ec7b7a7
Carlos Martín <cjmar87@gmail.com>
2018-10-30 15:53:36 -0600

    leve curso de vue js

---
+++ js/principal.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el : '#app',
+  data: {
+    lista: [
+      {nombre: 'Carlos' ,promedio: '10', estado: true},
+      {nombre: 'Liliana' ,promedio: '8', estado: true},
+      {nombre: 'Alberto' ,promedio: '6', estado: false}
+    ],
+    nombre: '',
+    promedio: ''
+  },
+  methods: {
+    agregarNota: function(){
+      
+    }
+  }
+})
